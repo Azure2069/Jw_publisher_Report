9870b6d186633c42db451ce4995cfe42a88f2c6b
Azure Isaac <isaacazure111@gmail.com>
2026-04-16 13:23:40 +0000

    publishers crud done

--- src/model/elder.py
+++ src/model/elder.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Float, Date, String
+from sqlalchemy import Column, Integer, Float, Date, String, ForeignKey
 from sqlalchemy.orm import relationship
 from database.database_init import Base
 
@@ -7,9 +7,9 @@ from database.database_init import Base
 class Elder(Base):
     __tablename__="Elders"
     elder_id=Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name=Column(String)
-    group=Column(Integer)
-    date_of_birth=Column(Date)
-    date_of_baptism=Column(Date)
+    user_id=Column(ForeignKey("publishers.id"))
+    group_id=Column(ForeignKey("groups.id"))
     managed_group=relationship("Groups", back_populates="overseer")
+    user_profile=relationship("All_users", back_populates="elder_profile")
+
 
--- src/model/users.py
+++ src/model/users.py
@@ -10,12 +10,15 @@ class All_Users(Base):
     __tablename__="publishers"
     id=Column(Integer, primary_key=True, index=True, autoincrement=True)
     name=Column(String)
-    group=Column(Integer)
     is_baptized=Column(Boolean)
     date_of_baptism=Column(Date)
     date_of_birth=Column(Date)
     gender=Column(String)
     role= Column(Enum(Roles))
+    group_id=Column(Integer, ForeignKey("groups.id"))
+    group=relationship("Groups", back_populates="user")
+    elder_profile=relationship("Elder", back_populates="user_profile")
+
 
 
 class Groups(Base):
@@ -24,3 +27,4 @@ class Groups(Base):
     name=Column(Integer)
     overseer_id=Column(Integer, ForeignKey("Elders.elder_id"))
     overseer=relationship("Elder", back_populates="managed_group")
+    user=relationship("All_users", back_populates="group")
